60c7c135d1c8d61b9f22a487db04a66d952e4d8b
tarex7 <tarex7@gmail.com>
2022-07-02 20:43:33 +0200

    Super bonus 1 + 2

--- script.js
+++ script.js
@@ -1,121 +1,147 @@
 let rows = 10;
 let cells = 10;
 let totalCells = rows * cells;
+
 let playerScore = 0;
 let bombs = 16;
 let maxScore = totalCells - bombs;
+
 let min = 1;
 let max = 100;
 
+let notAllowed;
+
 //Elementi UI
+const grid = document.getElementById("grid");
 const startBtn = document.getElementById("startBtn");
 const select = document.getElementById("level");
 const message = document.querySelector(".message");
 const gameOver = document.getElementById("game-over");
-const winMsg = document.getElementById("win-lose");
 const score = document.getElementById("score");
-const rndNumbers = [];
 
 //!FUNZIONI
 //!Funzione per creare 16 numeri casuali tutti diversi
-//
+
 const createRndNumbers = (min, max, howManyNumbers) => {
   //Genera numeri casuali finchè l'array non raggiunge la lunghezza richiesta
   while (rndNumbers.length < howManyNumbers) {
     const rnd = Math.floor(Math.random() * (max - min)) + min;
     //se il numero casuale non è nell'array lo mette
     if (rndNumbers.indexOf(rnd) === -1) rndNumbers.push(rnd);
-    console.log("min", min, "max", max, "howManyNumbers", howManyNumbers);
-    console.log(rndNumbers.sort());
+    //Ordina array bombe
+    rndNumbers.sort(function (a, b) {
+      return a - b;
+    });
   }
+  console.log("Cell bombs:", rndNumbers);
 };
 
-//!Crea cella
-const createCell = (content) => {
-  //Crea elemento
-  const cell = document.createElement("div");
-  //Aggiunge classe
-  cell.className = "cell";
+//!Funzione Crea cella
+const createCell = (totalCells) => {
+  for (let i = 1; i <= totalCells; i++) {
+    //Crea elemento div
+    const cell = document.createElement("div");
+    //Gli aggiunge classe "cell"
+    cell.className = "cell";
+    //Inserisce numero nella cella
+    cell.innerText = i;
+    //Aggiunge classe bomb se il numero della cella si trova nei rndNumbers
+    if (rndNumbers.includes(parseInt(cell.innerText)))
+      cell.classList.add("bomb");
 
-  //Inserisce numero cella
-  cell.innerText = content;
+    //! Logica click su cella
+    function onCellClick(e) {
+      //Previene click su celle già cliccate
+      if (e.target.className.includes("clicked") || notAllowed) {
+        return;
+      }
 
-  //Aggiunge event listener
+      //Controlla se viene cliccata una bomba
+      if (rndNumbers.includes(parseInt(e.target.innerText))) {
+        //Imposta sfondo rosso alla cella
+        e.target.style.backgroundColor = "red";
+        //Mostra messaggi
+        message.classList.add("show");
+        gameOver.innerText = "BOOOOM!";
+        score.innerText = `Hai totalizzato ${playerScore} punti`;
 
-  cell.addEventListener("click", (e) => {
-    if (e.target.className.includes("clicked")) {
-      return;
+        //!Funzione mostra bombe
+        function showBombs(cell) {
+          if (cell.classList.contains("bomb"))
+            cell.style.backgroundColor = "red";
+        }
+        allCells.forEach(showBombs);
+
+        //Blocca click su altre celle
+        notAllowed = true;
+      }
+
+      //Aggiunge classi
+      cell.classList.add("clicked", "not-allowed");
+
+      //Aumenta punteggio
+      playerScore++;
+      console.log(maxScore);
+      //Controlla se viene raggiunto il punteggio massimo
+      if (playerScore === maxScore) {
+        message.classList.add("show");
+        gameOver.innerText = "Hai vinto!";
+        score.innerText = `Hai totalizzato ${playerScore} punti`;
+        //Cambia colore sfondo
+        message.style.backgroundColor = "rgba(1, 133, 15, 0.6)";
+        //Blocca click su altre celle
+        notAllowed = true;
+      }
     }
-
-    if (rndNumbers.includes(parseInt(e.target.innerText))) {
-      e.target.style.backgroundColor = "red";
-      winMsg.innerText = "Hai perso";
-
-      score.innerText = `Hai totalizzato ${playerScore} punti`;
-      gameOver.innerText = "BOOOM!!!";
-
-      message.classList.add("show");
-    }
-
-    e.target.classList.add("clicked", "not-allowed");
-    playerScore++;
-
-    if (playerScore === totalCells - bombs) {
-      winMsg.innerText = "Hai vinto!!!";
-      score.innerText = `Hai totalizzato ${playerScore} punti`;
-
-      gameOver.innerText = "Game over";
-      message.classList.add("show");
-
-      message.style.backgroundColor = "rgba(1, 133, 15, 0.6)";
-    }
-
-    //Log numero cella
-    console.log(`Hai cliccato sulla cella numero ${e.target.innerText}`);
-  });
-
-  grid.appendChild(cell);
+    //Aggiunge evento alla cella
+    cell.addEventListener("click", onCellClick);
+    //Inserisce cella nella grid
+    grid.appendChild(cell);
+  }
+  //Lista bombe
+  const bombs = document.querySelectorAll(".bomb");
+  //Lista celle
+  const allCells = document.querySelectorAll(".cell");
 };
 
-//Event button
-
+//!START
 startBtn.addEventListener("click", () => {
   //Reset
   grid.innerHTML = "";
+  notAllowed = false;
+  rndNumbers = [];
   playerScore = 0;
   message.classList.remove("show");
 
-  //*Livelli difficoltà
-  //Normal
-  if (level.value == 2) {
-    totalCells = 81;
-    console.log(totalCells);
-    max = totalCells;
-    createRndNumbers(min, max, bombs);
+  //Difficulty levels
+  switch (parseInt(level.value)) {
+    //Easy
+    case 1:
+      createRndNumbers(1, 100, 16);
+      createCell(totalCells);
+      maxScore = totalCells - bombs;
+      break;
 
-    document.documentElement.style.setProperty("--rows", `9`);
-    document.documentElement.style.setProperty("--cells", `9`);
+    //Normal
+
+    case 2:
+      document.documentElement.style.setProperty("--rows", `9`);
+      document.documentElement.style.setProperty("--cells", `9`);
+      totalCells = 81;
+      createRndNumbers(1, 81, 16);
+      createCell(totalCells);
+      maxScore = totalCells - bombs;
+
+      break;
 
     //Hard
-  } else if (level.value == 3) {
-    totalCells = 49;
-    max = totalCells;
-    createRndNumbers(min, max, bombs);
+    case 3:
+      document.documentElement.style.setProperty("--rows", `7`);
+      document.documentElement.style.setProperty("--cells", `7`);
+      totalCells = 49;
+      maxScore = totalCells - bombs;
 
-    document.documentElement.style.setProperty("--rows", `7`);
-    document.documentElement.style.setProperty("--cells", `7`);
-
-    //Easy
-  } else if (level.value == 1) {
-    totalCells = 100;
-    max = totalCells;
-    createRndNumbers(min, max, bombs);
-
-    document.documentElement.style.setProperty("--rows", `10`);
-    document.documentElement.style.setProperty("--cells", `10`);
-  }
-
-  for (let i = 1; i <= totalCells; i++) {
-    createCell(i);
+      createRndNumbers(1, 49, 16);
+      createCell(totalCells);
   }
 });
